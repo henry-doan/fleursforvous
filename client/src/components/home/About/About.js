e5d643f87d552af01620f8a9f7388b8684028b81
Henry Doan <henrydoan96@gmail.com>
2022-05-06 00:35:04 -0600

    service and about content

--- client/src/components/home/About/About.js
+++ client/src/components/home/About/About.js
@@ -2,7 +2,10 @@ import { MainContainer } from "../../../styles/shared";
 
 const About = () => (
   <MainContainer color='Grey' id='About'>
-    About
+    <h1>About</h1>
+    <p>
+      After watching countless Korean dramas, I became inspired to recreate something so beautiful, and that was making flower arrangements for others. I did not know anyone or anywhere that was able to do that for me living here in Utah. This sparked the idea of incorporating this certain bouquet look into as a small local business. I have always liked helping others, and with the support of my family and friends, this idea became a dream come true. 
+    </p>
   </MainContainer>
 )
 
--- client/src/components/home/Services/Services.js
+++ client/src/components/home/Services/Services.js
@@ -2,7 +2,50 @@ import { MainContainer } from "../../../styles/shared";
 
 const Services = () => (
   <MainContainer id='Services'>
-    Services
+    <h1>
+      Services
+    </h1>
+    <ul>
+      <li>
+        Birthdays
+      </li>
+      <li>
+        Graduations
+      </li>
+      <li>
+        Anniversaries
+      </li>
+      <li>
+        Engagements
+      </li>
+      <li>
+        Dates
+      </li>
+      <li>
+        Baby Showers
+      </li>
+      <li>
+        Holidays
+      </li>
+      <li>
+        Parties
+      </li>
+      <li>
+        Just Because
+      </li>
+      <li>
+        Get Well Soon 
+      </li>
+      <li>
+        Thank You
+      </li>
+      <li>
+        Housewarming
+      </li>
+      <li>
+        Sympathy
+      </li>
+    </ul>
   </MainContainer>
 )
 
