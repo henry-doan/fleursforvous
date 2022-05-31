5dba299f5acae532dd8ff49035cf52dc95383ea3
Henry Doan <henrydoan96@gmail.com>
2022-05-30 21:31:42 -0600

    update policy

--- client/src/components/home/Header/Header.js
+++ client/src/components/home/Header/Header.js
@@ -4,7 +4,6 @@ import Logo from '../../../images/logo.jpg';
 
 const Header = () => (
   <MainContainer id="Header" color='Grey' size='950px' smsize='100vh' xssize='970px'>
-    {/* Fleurs For Vous */}
     <Parallax 
       bgImage='https://images.unsplash.com/photo-1627819797319-6e187b6e9762?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2313&q=80'
       bgImageAlt="bg" 
--- client/src/components/policies/Policy.js
+++ client/src/components/policies/Policy.js
@@ -5,43 +5,43 @@ const Policy = () => (
     <Container>
       <h1>WHO WE ARE</h1>
       <p>
-        Our website address is: www.fleursforvous.com.
+        Our URL address is www.fleursforvous.com.
       </p>
       <p>
         Our social media handle on all platforms is @fleursforvous.
       </p>
       <p>
-        All rights reserved on all content, styles, and code that are within the site and content posted.
+        All rights reserved on all content, styles, and code that are within the site.
       </p>
       <h1>
         WHAT PERSONAL DATA WE COLLECT AND WHY WE COLLECT IT
       </h1>
       <p>
-        When visitors fill out the inquiry form on the site we collect the data from the form to process the orders, we don't store any other infomation outside this form. If needed, we may ask for clarification or confirmation on the order details. The full name, email, phone, date needed by, time needed by and occasion is all required fields to process the order and to give information on who is making the order. All other fields are optional.
+      When you fill out the inquiry form on this site, we collect the data from the inquiry form to process your order(s). We do not store any other information outside of the inquiry form you have submitted to us. We may ask you for any clarifications on the order(s) details. Your full name, email address, phone number, date needed by, time needed by, and occasion are all required fields to process your order(s) and to provide information on who is making the order(s). All of the other fields are optional. 
       </p>
       <p>
-        You will get contacted by us to confirm and talk about the order. This is to customize each order based off your request and concerns and do so in a promptly way.
+      You will be contacted by us to confirm and discuss your order(s). This is to customize each of your orders based on your requests, questions, and concerns in a prompt manner.
       </p>
       <p>
-        We may ask, with your permission to use your social media handle to tag your product on our site or social media platforms. We will not grab any other infomation from your social media or use it for any other purpose.
+      We may ask, with your permission, to use your social media handle to tag your finished order(s) on our site or social media platforms. We do not grab any other information from your social media platforms or use them for any other purpose. 
       </p>
       <h3>
         Analytics
       </h3>
       <p>
-        We use the data from the inquiry form for our analytics to see when the most orders are being placed to see any trends for us to plan ahead and get materials in on time. We don't share this data with anyone and we only use the date the order has been placed.
+      We use the data from the inquiry form for our analytics to gather when the most orders are being placed as well as to see if there are any trends for us to plan ahead and purchase materials on time. We do not share this data with anyone else. We only use the date the orders have been placed. 
       </p>
       <h1>
         HOW LONG WE RETAIN YOUR DATA
       </h1>
       <p>
-        If you have a order with us, we will always remember and be grateful for working with you, the data we collect will still be stored within our system until it is removed or if the order is done. If you want us to remove the information provided, we will sure be happy to.
+        If you have ordered from us, the data from the inquiry form that has been collected will be stored within our system until it is removed or if the order has been completed. If you want us to remove the information you have provided us, we will surely be happy to. 
       </p>
       <h1>
         WHAT ABOUT PAYMENT
       </h1>
       <p>
-        We do not handle payment directly, we use third party tools of Venmo, Apple Pay and Cash app to use for payment of orders. All Payment and credit card information is all done so via those app, we do not store or keep track of your credit card information. Whatever you have questions or concerns within the app, you would contact the application itself. We don't have any control within their systems. We only get the payment and check off if the order is paid or not.
+      We use Venmo, Apple Pay, and Cash App as third party tools for payment of orders. We accept cash in physical form as our other option; however, a half deposit or full payment is required to place your order(s). All payment and credit card information is carried out via Venmo, Apple Pay, and Cash App, or through cash. We do not store or keep track of your credit card information. For all of your questions and concerns within those apps, you would contact the applications themselves. We only receive your payment(s) and verify whether or not your order(s) has been paid.
       </p>
     </Container>
   </>
