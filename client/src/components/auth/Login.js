51eae9def40b57fc0c85708e5614203bc1018457
Henry Doan <henrydoan96@gmail.com>
2022-06-17 08:51:05 -0600

    style login form

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { AuthConsumer } from "../../providers/AuthProvider";
+import { Container, Form, Button, Card } from 'react-bootstrap';
 
 const Login = ({ handleLogin }) => {
   const [user, setUser] = useState({ email: '', password: '' })
@@ -10,31 +11,46 @@ const Login = ({ handleLogin }) => {
   }
   
   return (
-    <>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
-        <label>Email</label>
-        <input
-          type="email"
-          autoFocus
-          required         
-          name='email'
-          value={user.email}
-          placeholder='Email'
-          onChange={(e) => setUser({ ...user, email: e.target.value })}
-        />
-        <label>Password</label>
-        <input
-          required
-          name='password'
-          value={user.password}
-          placeholder='Password'
-          type='password'
-          onChange={(e) => setUser({ ...user, password: e.target.value })}
-        />
-        <button type='submit'>Submit</button>
-      </form>
-    </>
+    <Container style={{ display: 'flex', justifyContent: 'center',  }}>
+      <Card style={{ width: '24rem', marginTop: 150 }}>
+        <Card.Body className='text-center'>
+          <Card.Title>Login</Card.Title>
+          <Card.Text>
+            <Form onSubmit={handleSubmit}>
+              <br />
+              <Form.Group className="mb-3">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  type="email"
+                  autoFocus
+                  required         
+                  name='email'
+                  value={user.email}
+                  placeholder='Email'
+                  onChange={(e) => setUser({ ...user, email: e.target.value })} 
+                />
+              </Form.Group>
+              <br />
+              <Form.Group className="mb-3">
+                <Form.Label>Password</Form.Label>
+                <Form.Control 
+                  required
+                  name='password'
+                  value={user.password}
+                  placeholder='Password'
+                  type='password'
+                  onChange={(e) => setUser({ ...user, password: e.target.value })} 
+                />
+              </Form.Group>
+              <br />
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Container>
   )  
 }
 
